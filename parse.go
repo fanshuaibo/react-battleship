f84c7147409c504e475a012139ec876ae0e1c9c3
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-11-18 08:48:50 +0000

    Modified README

--- parse.go
+++ parse.go
@@ -2,3 +2,17 @@
 package pgoutput
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/pgtype"
+)
+
+type decoder struct {
+	order binary.ByteOrder
+	buf   *bytes.Buffer
+}
+
+func (d *decoder) bool() bool {
