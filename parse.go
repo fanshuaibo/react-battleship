ed4bc5e02b38618aa9a72c02779af7694b1d2199
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-11-22 06:20:21 +0000

    Rename stuff

--- parse.go
+++ parse.go
@@ -46,3 +46,25 @@ func (d *decoder) uint32() uint32 {
 	return x
 
 }
+
+func (d *decoder) uint64() uint64 {
+	x := d.order.Uint64(d.buf.Next(8))
+	return x
+}
+
+func (d *decoder) int8() int8   { return int8(d.uint8()) }
+func (d *decoder) int16() int16 { return int16(d.uint16()) }
+func (d *decoder) int32() int32 { return int32(d.uint32()) }
+func (d *decoder) int64() int64 { return int64(d.uint64()) }
+
+func (d *decoder) timestamp() time.Time {
+	micro := int(d.uint64())
+	ts := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	return ts.Add(time.Duration(micro) * time.Microsecond)
+}
+
+func (d *decoder) rowinfo(char byte) bool {
+	if d.buf.Next(1)[0] == char {
+		return true
+	} else {
+		d.buf.UnreadByte()
