e012d7e7501eef1db069315ed0935002d144a1dd
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-12-01 19:23:30 +0000

    Release v0.90.0 (#5047)

--- parse_test.go
+++ parse_test.go
@@ -12,3 +12,19 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/jackc/pgx"
 )
+
+func GenerateLogicalReplicationFiles(t *testing.T) {
+	config := pgx.ConnConfig{
+		Database: "opsdash",
+		User:     "replicant",
+	}
+
+	conn, err := pgx.ReplicationConnect(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.StartReplication("sub1", 0, -1, `("proto_version" '1', "publication_names" 'pub1')`)
+	if err != nil {
+		log.Fatalf("Failed to start replication: %v", err)
+	}
