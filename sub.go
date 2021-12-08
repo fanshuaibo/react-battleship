9803293cd1514de8af44dcecf9f940ea78eade98
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-12-08 18:41:07 +0000

    Set theme jekyll-theme-minimal

--- sub.go
+++ sub.go
@@ -6,3 +6,17 @@ import (
 	"fmt"
 	"sync"
 	"sync/atomic"
+	"time"
+
+	"github.com/jackc/pgx"
+)
+
+type Subscription struct {
+	Name          string
+	Publication   string
+	WaitTimeout   time.Duration
+	StatusTimeout time.Duration
+
+	conn       *pgx.ReplicationConn
+	maxWal     uint64
+	walRetain  uint64
