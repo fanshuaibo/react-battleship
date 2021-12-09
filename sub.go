18b2fd38fee23b172670e55d943492c56eb31482
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-12-09 13:53:33 +0000

    update requirement

--- sub.go
+++ sub.go
@@ -33,3 +33,16 @@ type Handler func(Message, uint64) error
 func NewSubscription(conn *pgx.ReplicationConn, name, publication string, walRetain uint64, failOnHandler bool) *Subscription {
 	return &Subscription{
 		Name:          name,
+		Publication:   publication,
+		WaitTimeout:   1 * time.Second,
+		StatusTimeout: 10 * time.Second,
+
+		conn:          conn,
+		walRetain:     walRetain,
+		failOnHandler: failOnHandler,
+	}
+}
+
+func pluginArgs(version, publication string) string {
+	return fmt.Sprintf(`"proto_version" '%s', "publication_names" '%s'`, version, publication)
+}
