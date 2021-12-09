268d97c9df6fcaa9bdf4d7671111a0667f76221a
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-12-09 13:15:42 +0000

    Removed debugging lines

--- sub.go
+++ sub.go
@@ -20,3 +20,16 @@ type Subscription struct {
 	conn       *pgx.ReplicationConn
 	maxWal     uint64
 	walRetain  uint64
+	walFlushed uint64
+
+	failOnHandler bool
+
+	// Mutex is used to prevent reading and writing to a connection at the same time
+	sync.Mutex
+}
+
+type Handler func(Message, uint64) error
+
+func NewSubscription(conn *pgx.ReplicationConn, name, publication string, walRetain uint64, failOnHandler bool) *Subscription {
+	return &Subscription{
+		Name:          name,
