a7f6496060faedce9dc8e32ddb1356034a83315b
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2022-01-14 10:27:07 +0000

    Force rebuild

--- values.go
+++ values.go
@@ -14,4 +14,4 @@ type RelationSet struct {
 }
 
 // NewRelationSet creates a new relation set.
-// Optionally ConnInfo can be provided, however currently we need some c
+// Optionally ConnInfo can be provided, however currently we need some changes to pgx
