5455f6acdabadb13d627952cdf07b67d69cec7d0
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2022-03-21 21:01:25 +0000

    Const'ify

--- values.go
+++ values.go
@@ -42,4 +42,4 @@ func (rs *RelationSet) Values(id uint32, row []Tuple) (map[string]pgtype.Value, 
 		decoder := col.Decoder()
 
 		if err := decoder.DecodeText(rs.connInfo, tuple.Value); err != nil {
-			return nil, fmt.Err
+			return nil, fmt.Errorf("error de
