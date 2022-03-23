3f08dc220234ecb84c9e5f4dcb9791bf4afa0340
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2022-03-23 08:10:53 +0000

    Changed template

--- values.go
+++ values.go
@@ -42,4 +42,4 @@ func (rs *RelationSet) Values(id uint32, row []Tuple) (map[string]pgtype.Value, 
 		decoder := col.Decoder()
 
 		if err := decoder.DecodeText(rs.connInfo, tuple.Value); err != nil {
-			return nil, fmt.Errorf("error decoding tuple %d
+			return nil, fmt.Errorf("error decoding tuple %d: %s", i, e
