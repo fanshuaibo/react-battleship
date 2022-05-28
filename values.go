175660449126fa5015a9a38df7334b9dd41cc8b6
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2022-05-28 07:48:58 +0000

    Update todo list

--- values.go
+++ values.go
@@ -72,4 +72,5 @@ func (c Column) Decoder() DecoderValue {
 	case pgtype.CIDROID:
 		return &pgtype.CIDR{}
 	case pgtype.CharOID:
-		// Not all possible values of QChar are representable in the text 
+		// Not all possible values of QChar are representable in the text format
+		ret
